8dea9b5c1b851cdb6fcd79bf318a30053fe715c0
Leonardo <leonardo@llamapress.ai>
2025-08-24 15:33:09 -0700

    Make it easy for LlamaBot to access important debug information from the iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,44 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :authenticate_user!
   before_action :allow_iframe_requests
+  before_action :set_context
 
   def allow_iframe_requests
     response.headers.delete('X-Frame-Options')
   end
+
+  def set_context
+    @request_path = request.path
+    @view_path = resolve_view_path #this is used for LlamaBot to know what file to write code changes to.
+    
+    # Prevent caching to ensure fresh request_path and view_path on every request
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+  end
+
+  private
+
+  def resolve_view_path
+    route = Rails.application.routes.recognize_path(request.path, method: request.method)
+    controller = route[:controller]
+    action = route[:action]
+
+    # Check if there's a specific route helper for this path
+    route_helper = Rails.application.routes.named_routes.helper_names.find do |helper|
+      path = send("#{helper}_path") rescue nil
+      path == request.path
+    end
+
+    if route_helper
+      # If a route helper is found, use it to determine the view
+      controller, action = route_helper.to_s.sub(/_path$/, '').split('_', 2)
+    end
+
+    "app/views/#{controller}/#{action}.html.erb"
+  rescue ActionController::RoutingError
+    nil
+  end
+
+
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,44 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import * as ActionCable from "@rails/actioncable"
 window.ActionCable = ActionCable
+
+console.log("application.js loaded!!");
+
+window.addEventListener("message", (event) => {
+    console.log("here is the message", event);
+    
+    // Ensure we have the most up-to-date HTML content
+    window.full_html = document.documentElement.outerHTML;
+    
+    console.log("full_html", window.full_html);
+    console.log("request_path", window.request_path);
+    console.log("view_path", window.view_path);
+    console.log("page_loaded_at", window.page_loaded_at);
+
+    // Validate that we have current data
+    if (!window.request_path || !window.view_path) {
+        console.warn("Missing request_path or view_path - page may not be fully loaded");
+    }
+
+    //note: we can use html2canvas to feed the screenshot to Leonardo
+    // html2canvas(document.body).then(canvas => {
+    //     const pngData = canvas.toDataURL("image/png"); // base64 encoded PNG
+    //     console.log(pngData); // "data:image/png;base64,iVBORw0K..."
+    //     event.source.postMessage({ 
+    //         source: 'llamapress', 
+    //         full_html: window.full_html, 
+    //         request_path: window.request_path, 
+    //         view_path: window.view_path, 
+    //         page_loaded_at: window.page_loaded_at,
+    //         screenshot: pngData 
+    //     }, event.origin);
+    //   });
+    
+    event.source.postMessage({ 
+        source: 'llamapress', 
+        full_html: window.full_html, 
+        request_path: window.request_path, 
+        view_path: window.view_path,
+        page_loaded_at: window.page_loaded_at
+    }, event.origin);
+});
